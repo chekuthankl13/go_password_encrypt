ddfcff534f512521708f82e3023ca97a98c7cce1
chekuthankl13 <chekuthankl13@gmail.com>
2022-11-07 12:13:00 +0530

    go password encrpty

---
+++ go.mod
@@ -0,0 +1,5 @@
+module hashing
+
+go 1.19
+
+require golang.org/x/crypto v0.1.0
---
+++ hashing/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+
+	
+)
+
+func main() {
+
+
+	///////// hashing /////
+
+// 	password := []byte("")
+// 	// password1 := []byte("")
+
+// 	hashedpassword, err := bcrypt.GenerateFromPassword(password, bcrypt.MinCost)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Println(string(hashedpassword))
+// // err = 
+// 	err = bcrypt.CompareHashAndPassword(hashedpassword, password)
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+
+	///////////// end ////////////
+
+
+////// sha256 encrpty ///////////////////
+
+	s := "------"
+
+	h := sha256.New()
+
+	h.Write([]byte(s))
+
+	bs := h.Sum(nil)
+
+	fmt.Println(s)
+
+	fmt.Printf("%x\n",bs)
+
+///////////// end /////////
+}
